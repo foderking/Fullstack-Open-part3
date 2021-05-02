ee173ae3565656dd1555d8eb90c8de43e2f4d9b5
fenix <freatshow@gmail.com>
2021-05-03 00:20:54 +0100

    completed exercise 3.15 - 3.18

--- index.js
+++ index.js
@@ -20,12 +20,15 @@ const Note = require('./models/mongo')
 
 app.use(express.json())
 
+app.use(express.static('build'))
+
+
 if (PORT == 3001) {
 	var morgan = require('morgan')
 
 	app.use(
 		morgan(function (tokens, req, res) {
-			if (tokens.method(req, res) === "POST") {
+			if (["POST", "PUT"].includes(tokens.method(req, res))) {
 			  return  `${tokens.method(req, res)} "${tokens.url(req, res)}" | User-Agent ${tokens['user-agent'](req, res)} \n ${white} ${tokens.status(req, res)} ${blue} - ${tokens['response-time'](req, res)} ms \n${cyan} ${JSON.stringify(req.body)} ${blue}` 
 			}
 			else {
@@ -35,39 +38,10 @@ if (PORT == 3001) {
 	)
 }
 
-app.use(express.static('build'))
-
-
-
 
 
 app.listen(PORT, () => {
 	console.log(FgYellow, `Server listening on port: ${PORT}`, FgBlue)
-})
-
-
-app.get('/api/persons', (request, response) => {
-  Note
-    .find({})
-    .then(note => {
-      response.json(note)
-    })
-		.catch(error => {
-			response.send(error.message)
-		})
-})
-
-app.get('/api/persons/:id', (request, response) => {
-	const id = request.params.id
-
-	Note
-		.findById(id)
-		.then(note => {
-			response.json(note)
-		})
-		.catch(error => {
-			response.send(error.message)
-		})
 })
 
 app.get('/info', (request, response) => {
@@ -79,19 +53,36 @@ app.get('/info', (request, response) => {
 	)
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
+  Note
+    .find({})
+    .then(note => {
+      response.json(note)
+    })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
 	const id = request.params.id
 
-	if (usedId.includes(id)) {
-		notes = notes.filter(each => each.id !== id)	
-		response.status(204).json(notes)
-	} 
-	else {
-		response.status(404).json({"error": "id does not exist"})
-	}
+	Note
+		.findById(id)
+		.then(note => {
+			response.json(note)
+		})
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
+  Note
+  	.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).json(result)
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
 	const body = request.body
 
 	if ( !body.name || !body.number ) {
@@ -109,26 +100,44 @@ app.post('/api/persons', (request, response) => {
   		.then(saved => {
 		  	response.json(saved)
   		})
+   		.catch(error => next(error))
   }
 })
 
-app.put('/api/persons/:id', (request, response) => {
-	const body = request.body
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
-	if ( !body.name || !body.number ) {
-    response.status(400).json({ 
-      error: 'content missing' 
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
     })
+    .catch(error => next(error))
+})
+
+
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+
+
+const errorHandler = (error, request, response, next) => {
+
+  if (error.name === 'CastError' | error.name === "ValidationError") {
+    return response.status(400).send({ error: 'malformatted id' })
   } 
   else {
-  	const note = new Note({
-			name: body.name,
-			number: body.number,
-  	})
-  	note
-  		.save()
-  		.then(saved => {
-		  	response.json(saved)
-  		})
+  console.error(error.message)
   }
-})
+
+  next(error)
+}
+app.use(errorHandler)
--- test.js
+++ test.js
@@ -1,8 +1,18 @@
 require('dotenv').config()
 const Note = require('./models/mongo')
 
+let notes = []
 Note
   .find({})
-  .then(notes => {
-    console.log(notes)
+  .then(res => {
+    notes = notes.concat(res)
+    notes = notes.map(each => each._id)
+    notes.forEach(each => {
+    	Note
+    		.findById(each)
+    		.then(response => {
+		    	console.log(response)
+    		})
+    })
   })
+
